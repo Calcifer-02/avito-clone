abac104fb6fd792ceb04b128d5fed7767165eab6
Calcifer-02 <02.calcifer@gmail.com>
2025-02-16 19:28:40 +0300

    F

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ const AppFooter = () => {
    return (
       <Footer style={{ textAlign: "center", marginTop: "50px" }}>
          Avito Clone ©2025 Created by
-         <a href="https://t.me/Calcifer_02"> @Calcifer-02</a>
+         <a target="_blank" href="https://t.me/Calcifer_02">
+            {" "}
+            @Calcifer-02
+         </a>
       </Footer>
    );
 };
